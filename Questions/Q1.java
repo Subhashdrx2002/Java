61e754fa6e8221278507e8887fce7188473596c2
Subhash <123473222+Subhashdrx2002@users.noreply.github.com>
2024-03-29 04:01:42 +0000

    # new file

--- Questions/Q1.java
+++
@@ -1,9 +0,0 @@
-class Q1{
-    public static void main(String[]args){
-        System.out.println("hello world");
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
-    }
-}
---
+++ Questions/RightHalfPyramid.java
@@ -0,0 +1,9 @@
+class RightHalfPyramid{
+    public static void main(String[]args){
+        System.out.println("hello world");
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+    }
+}
