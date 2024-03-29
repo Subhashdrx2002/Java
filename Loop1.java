fdfb2457168386e66b230c9c287b1e537ab6e55e
Subhash <123473222+Subhashdrx2002@users.noreply.github.com>
2024-03-29 03:36:53 +0000

    # New file

---
+++ Loop1.java
@@ -0,0 +1,10 @@
+public class Loop1 {
+    public static void main(String[] args) {
+        // for loop
+        int i;
+        for (i = 0; i < 5; i++) {
+            System.out.println(i);
+           
+        }
+    }
+}
---
+++ Loop2.java
@@ -0,0 +1,15 @@
+public class Loop2 {
+    public static void main(String[] args) {
+        // declare a loop variable
+        int i = 0;
+
+        // start the while loop
+        while (i < 5) {
+            // print the message
+            System.out.println(i);
+
+            // increment the loop variable
+            i++;
+        }
+    }
+}
